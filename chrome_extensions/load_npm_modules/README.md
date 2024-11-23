7cc13a5aba4b7d99de91b86c38e4de17d5071908
Devin Chenyang <devin.chenyang@grabtaxi.com>
2024-11-23 09:57:21 +0800

    Add load_npm_modules example with package.json, README, and background script

---
+++ chrome_extensions/load_npm_modules/README.md
@@ -0,0 +1,5 @@
+# Example: How to load a module from npm packages
+
+1. `npm install`
+2. Install Chrome extension
+3. Check DevTool Console of service worker, you will see a message "An example to show how to load a module from npm packages", and definition of `formatDate` function.
---
+++ chrome_extensions/load_npm_modules/backgroundScripts/index.js
@@ -0,0 +1,10 @@
+import { formatDate } from '../node_modules/@db-man/github/es6/utils.js';
+
+console.log('[load_npm_modules] Background script started.');
+
+console.log(
+  'An example to show how to load a module from npm packages:',
+  formatDate
+);
+
+console.log('[load_npm_modules] Background script ended.');
