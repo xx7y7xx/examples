88dd35628b03f3e3e372929297b03e7c725297fa
Devin Chenyang <devin.chenyang@grabtaxi.com>
2024-12-09 18:06:39 +0800

    Initialize batcher project with main functionality and README

---
+++ nodejs/batcher/README.md
@@ -0,0 +1,12 @@
+# batcher
+
+```sh
+npx ts-node main.ts
+```
+
+Will print
+
+```
+Fetching users with params: { id_in: [ 1, 2 ] }
+Fetching users with params: { id_in: [ 3, 4 ] }
+```
---
+++ nodejs/batcher/main.ts
@@ -0,0 +1,46 @@
+import { create, keyResolver, windowScheduler } from '@yornaath/batshit';
+
+type User = { id: number; name: string };
+
+const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+// simulate a client function that fetches users from a database
+const client_users_where = (params: { id_in: number[] }) =>
+  new Promise<User[]>((resolve) => {
+    setTimeout(() => {
+      console.log('Fetching users with params:', params);
+      resolve([]);
+    }, 500);
+  });
+
+const users = create({
+  fetcher: async (ids: number[]) => {
+    return client_users_where({
+      id_in: ids,
+    });
+  },
+  resolver: keyResolver('id'),
+  scheduler: windowScheduler(10), // Default and can be omitted.
+});
+
+const main = async () => {
+  /**
+   * Requests will be batched to one call since they are done within the same time window of 10 ms.
+   */
+  const bob = users.fetch(1);
+  const alice = users.fetch(2);
+
+  const bobUndtAlice = await Promise.all([bob, alice]);
+
+  await delay(100);
+
+  /**
+   * New Requests will be batched in a another call since not within the first timeframe.
+   */
+  const joe = users.fetch(3);
+  const margareth = users.fetch(4);
+
+  const joeUndtMargareth = await Promise.all([joe, margareth]);
+};
+
+main();
