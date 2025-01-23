4a08f4eb72a77438c7eda4a7dfdd3dddaf1c1e1c
Devin Chenyang <devin.chenyang@grabtaxi.com>
2025-01-23 14:46:41 +0800

    feat: refactor line chart implementation into separate LineExample component

--- web/ant_design_plots_v1/src/Application/index.tsx
+++ web/ant_design_plots_v1/src/Application/index.tsx
@@ -1,36 +1,9 @@
 import React from 'react';
-import { Line, LineConfig } from '@ant-design/plots';
+
+import LineExample from './LineExample';
 
 const Application = () => {
-  const data = [
-    {
-      Date: '2016-11',
-      scales: 278,
-    },
-    {
-      Date: '2016-12',
-      scales: 195,
-    },
-    {
-      Date: '2017-01',
-      scales: 145,
-    },
-    {
-      Date: '2017-02',
-      scales: 207,
-    },
-  ];
-  const config: LineConfig = {
-    data,
-    padding: 'auto',
-    xField: 'Date',
-    yField: 'scales',
-    xAxis: {
-      // type: 'timeCat',
-      tickCount: 5,
-    },
-  };
-  return <Line {...config} />;
+  return <LineExample />;
 };
 
 export default Application;
