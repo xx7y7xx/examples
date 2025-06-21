090d3846b51ff67c7f777b0d5e5edeec441d0e71
Devin Chenyang <devin.chenyang@grabtaxi.com>
2025-06-21 22:03:23 +0800

    change loki payload

--- nodejs/grafana_loki/send_log.js
+++ nodejs/grafana_loki/send_log.js
@@ -22,7 +22,7 @@ async function sendLog(level, message, labels = {}) {
       streams: [
         {
           stream: {
-            job: "train_scraping",
+            job: "examples__nodejs__grafana_loki",
             level,
             ...labels,
           },
